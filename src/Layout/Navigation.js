18efedc5d1dbb336553956cfc9a98c1914c43d51
Kng Jefas <58780463+NKANGIJAFARI@users.noreply.github.com>
2021-05-29 11:12:58 +0400

    Added layout of the navigation

--- src/Layout/Navigation.js
+++ src/Layout/Navigation.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from 'react';
 
 const Navigation = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <nav id='nav-wrap' onClick={handleActive}>
+      <div className='navbar'>
+        <a href='#home' className='logo'>
+          <img src='/images/logo.png' alt='nkangi logo' />
+        </a>
 
-export default Navigation
+        <ul id='nav' className={`${active ? 'active' : 'inactive'} nav`}>
+          <li className='current'>
+            <a className='smoothscroll' href='#home'>
+              <FontAwesomeIcon icon={faHome} /> Home
+            </a>
+          </li>
+          <li className='aboutNav'>
+            <a className='smoothscroll' href='#about'>
+              <FontAwesomeIcon icon={faUserTie} /> About
+            </a>
+          </li>
+          <li>
+            <a className='smoothscroll' href='#services'>
+              <FontAwesomeIcon icon={faNetworkWired} /> Services
+            </a>
+          </li>
+          <li>
+            <a className='smoothscroll' href='#skills'>
+              <FontAwesomeIcon icon={faCode} /> Skills
+            </a>
+          </li>
+          <li>
+            <a className='smoothscroll' href='#portfolio'>
+              <FontAwesomeIcon icon={faBriefcase} /> Works
+            </a>
+          </li>
+          <li>
+            <a className='smoothscroll' href='#testimonials'>
+              <FontAwesomeIcon icon={faUsers} /> Testimonials
+            </a>
+          </li>
+          <li>
+            <a className='smoothscroll' href='#contact'>
+              <FontAwesomeIcon icon={faIdCard} /> Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
